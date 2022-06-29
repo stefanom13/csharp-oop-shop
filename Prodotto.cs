aafe304923b4f432a3c145788297cf5c6e8a8e8a
stefanom13 <stefanomancuso13@gmail.com>
2022-06-29 14:23:49 +0200

    Aggiungere i file di progetto.

---
+++ Prodotto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Shop
+{
+    internal class Prodotto
+    {
+        public int codice;
+        public string nome;
+        public string descrizione;
+        public float prezzo;
+        public int iva;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Esercizio di oggi: C# Shop nome repo: csharp-oop-shop
+//Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+//Un prodotto è caratterizzato da:
+//codice(numero intero)
+//nome
+//descrizione
+//prezzo
+//iva Usate
+//opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+//Il codice prodotto sia accessibile solo in lettura
+//Gli altri attributi siano accessibili sia in lettura che in scrittura
+//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+//Il prodotto esponga un metodo per avere il nome esteso,
+//ottenuto concatenando codice + nome Nella vostro programma principale, testate tutte le funzionalità della classe Prodotto.
+//BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+
+
