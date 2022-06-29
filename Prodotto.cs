bb99603b1dd90812c26f0c45f840a0c40e078e20
stefanom13 <stefanomancuso13@gmail.com>
2022-06-29 14:43:07 +0200

    classe implementata

--- Prodotto.cs
+++ Prodotto.cs
@@ -8,10 +8,44 @@ namespace Csharp_Shop
 {
     internal class Prodotto
     {
-        public int codice;
-        public string nome;
-        public string descrizione;
-        public float prezzo;
-        public int iva;
+        public int Codice { get; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        private double prezzo;
+        private double iva;
+
+        // get e set di prezzo e iva
+        public double Prezzo
+        {
+            get { return prezzo; }
+            set
+            {
+                prezzo = value;
+                iva = prezzo * 0.21;
+            }
+        }
+
+        public double Iva
+        {
+            get { return iva; }
+        }
+
+        //costruttore
+        public Prodotto(string nome, string descrizione, double prezzo)
+        {
+            Codice = new Random().Next(1, 99999999);
+            Nome = nome;
+            Descrizione = descrizione;
+            Prezzo  = prezzo;
+        }
+
+        public string GetNomeEsteso()
+        {
+            return $"{Codice} - {Nome}";
+        }
+        public double GetPrezzoIva()
+        {
+            return prezzo + iva;
+        }
     }
 }
